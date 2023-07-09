1f82443810e940c79617375e986b4c8d7a6ecfc6
Amarkharvi <amarkharvi316@gmail.com>
2023-07-10 00:04:23 +0530

    fix

--- components/MainContainer.js
+++ components/MainContainer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const MainContainer = ({ children }) => {
     return (
-        <div className=" bg-white shadow-lg my-10 w-3/4 ">
+        <div className=" bg-white shadow-lg my-10 lg:w-3/4 w-96 ">
             {children}
         </div>
     );
